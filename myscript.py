f4f561ebd2224953cc4d609aff2ce186a3d3e920
nadialabad <nadia.labad@polymtl.ca>
2024-11-08 21:24:08 -0500

    6

---
+++ myscript.py
@@ -0,0 +1,10 @@
+import os
+
+badhash = "c1a4be04b972b6c17db242fc37752ad517c29402"
+goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
+
+# Start git bisect with the specified bad and good commits
+os.system(f"git bisect start {badhash} {goodhash}")
+
+# Run the tests automatically on each commit in the bisect range
+os.system("git bisect run python manage.py test")
